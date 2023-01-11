2ee64e8d3763d0a61b1e371b7328cf45d67bf852
Weslei Rodrigues <wesleiralves@gmail.com>
2023-01-11 06:39:25 -0300

    Exercicios de vetores e Orientação a Objeto

---
+++ Orientacao_Objeto/Ativ_Vetor01/Ativ_Vetor01/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Ativ_Vetor01
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set;}
+      
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;           
+        }            
+
+        
+    }
+}
---
+++ Orientacao_Objeto/Ativ_Vetor01/Ativ_Vetor01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Transactions;
+
+namespace Ativ_Vetor01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Entre com a quantidade de produtos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] v = new Produto[n];
+            
+            for(int i=0; i<n; i++)
+            {
+                Console.Write("Entre com o nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Entre com o preço do produto: ");
+                double preco = double.Parse(Console.ReadLine());
+                v[i] = new Produto(nome, preco);
+            }
+            double soma = 0;
+            for(int i=0; i<n ; i++)
+            {
+                soma += v[i].Preco;
+            }
+            double media = soma / n;
+
+            Console.WriteLine("A média dos produtos é: R$ " + media.ToString("F2"));
+        }
+    }
+}
